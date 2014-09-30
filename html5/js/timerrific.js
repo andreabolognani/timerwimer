c73dc088f75582b9393b5ef0d1873370e8d44e62
Andrea Bolognani <eof@kiyuko.org>
2014-10-01 01:11:53 +0200

    Introduce the timerrific namespace.

--- html5/js/timerrific.js
+++ html5/js/timerrific.js
@@ -16,229 +16,159 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-var Timer = function()
-{
-	this._id = 0;
-	this._label = "";
-	this._state = Timer.State.STOPPED;
+var timerrific = {};
 
-	this._targetSeconds = 0;
-	this._elapsedSeconds = 0;
-	this._remainingSeconds = 0;
-};
+(function() {
 
-Timer.State =
-{
-	STOPPED:  "STOPPED",
-	RUNNING:  "RUNNING",
-	PAUSED:   "PAUSED",
-	FINISHED: "FINISHED"
-};
+	// Timer class
 
-Timer.now = function()
-{
-	return new Date().getTime();
-};
+	var Timer;
 
-Timer.prototype.start = function()
-{
-	if (this.getState() == Timer.State.STOPPED ||
-	    this.getState() == Timer.State.PAUSED)
+	Timer = function()
 	{
-		this._state = Timer.State.RUNNING;
+		this._id = 0;
+		this._label = "";
+		this._state = Timer.State.STOPPED;
 
-		this._lastUpdateTime = Timer.now();
+		this._targetSeconds = 0;
+		this._elapsedSeconds = 0;
+		this._remainingSeconds = 0;
+	};
 
-		this._interval = setInterval(this._update.bind(this), 100);
-	}
-};
-
-Timer.prototype.pause = function()
-{
-	if (this.getState() == Timer.State.RUNNING)
+	Timer.State =
 	{
-		this._state = Timer.State.PAUSED;
+		STOPPED:  "STOPPED",
+		RUNNING:  "RUNNING",
+		PAUSED:   "PAUSED",
+		FINISHED: "FINISHED"
+	};
+
+	Timer.now = function()
+	{
+		return new Date().getTime();
+	};
+
+	Timer.prototype.start = function()
+	{
+		if (this.getState() == Timer.State.STOPPED ||
+		    this.getState() == Timer.State.PAUSED)
+		{
+			this._state = Timer.State.RUNNING;
+
+			this._lastUpdateTime = Timer.now();
+
+			this._interval = setInterval(this._update.bind(this), 100);
+		}
+	};
+
+	Timer.prototype.pause = function()
+	{
+		if (this.getState() == Timer.State.RUNNING)
+		{
+			this._state = Timer.State.PAUSED;
+
+			clearInterval(this._interval);
+			this._interval = null;
+		}
+	};
+
+	Timer.prototype._finish = function()
+	{
+		this._state = Timer.State.FINISHED;
 
 		clearInterval(this._interval);
 		this._interval = null;
-	}
-};
+	};
 
-Timer.prototype._finish = function()
-{
-	this._state = Timer.State.FINISHED;
-
-	clearInterval(this._interval);
-	this._interval = null;
-};
-
-Timer.prototype.stop = function()
-{
-	this._state = Timer.State.STOPPED;
-
-	this._elapsedSeconds = 0;
-	this._remainingSeconds = this.getTargetSeconds();
-
-	clearInterval(this._interval);
-	this._interval = null;
-};
-
-Timer.prototype._update = function()
-{
-	// If the timer has been last updated a second ago, update it again
-	if (Math.floor((Timer.now() - this._lastUpdateTime) / 1000) > 0)
+	Timer.prototype.stop = function()
 	{
-		this._elapsedSeconds += 1;
-		this._remainingSeconds = this.getTargetSeconds() - this.getElapsedSeconds();
+		this._state = Timer.State.STOPPED;
 
-		// Increase by a full second instead of using the current
-		// time to compensate for JavaScript timer drift
-		this._lastUpdateTime += 1000;
-	}
+		this._elapsedSeconds = 0;
+		this._remainingSeconds = this.getTargetSeconds();
 
-	if (this.getElapsedSeconds() >= this.getTargetSeconds())
+		clearInterval(this._interval);
+		this._interval = null;
+	};
+
+	Timer.prototype._update = function()
 	{
-		this._finish();
-	}
-};
+		// If the timer has been last updated a second ago, update it again
+		if (Math.floor((Timer.now() - this._lastUpdateTime) / 1000) > 0)
+		{
+			this._elapsedSeconds += 1;
+			this._remainingSeconds = this.getTargetSeconds() - this.getElapsedSeconds();
 
-Timer.prototype.setId = function(id)
-{
-	this._id = id;
-};
+			// Increase by a full second instead of using the current
+			// time to compensate for JavaScript timer drift
+			this._lastUpdateTime += 1000;
+		}
 
-Timer.prototype.getId = function()
-{
-	return this._id;
-};
+		if (this.getElapsedSeconds() >= this.getTargetSeconds())
+		{
+			this._finish();
+		}
+	};
 
-Timer.prototype.setLabel = function(label)
-{
-	this._label = label;
-};
-
-Timer.prototype.getLabel = function()
-{
-	return this._label;
-};
-
-Timer.prototype.setTargetSeconds = function(targetSeconds)
-{
-	this._targetSeconds = targetSeconds;
-
-	// Changing the target time causes the timer to stop
-	this.stop();
-};
-
-Timer.prototype.getTargetSeconds = function()
-{
-	return this._targetSeconds;
-};
-
-Timer.prototype.getState = function()
-{
-	return this._state;
-};
-
-Timer.prototype.getElapsedSeconds = function()
-{
-	return this._elapsedSeconds;
-};
-
-Timer.prototype.getRemainingSeconds = function()
-{
-	return this._remainingSeconds;
-};
-
-function setupInterface(timers)
-{
-	var box;
-	var text;
-	var span;
-	var br;
-
-	for (var i = 0; i < timers.length; i++)
+	Timer.prototype.setId = function(id)
 	{
-		var timer;
-		var id;
+		this._id = id;
+	};
 
-		timer = timers[i];
-		id = timer.getId();
-
-		box = document.createElement("div");
-		box.setAttribute("id", "timer:" + id);
-		document.getElementById("container")
-			.appendChild(box);
-
-		text = document.createTextNode("Id: ");
-		span = document.createElement("span");
-		span.id = "id:" + id;
-		br = document.createElement("br");
-		box.appendChild(text);
-		box.appendChild(span);
-		box.appendChild(br);
-
-		text = document.createTextNode("Label: ");
-		span = document.createElement("span");
-		span.id = "label:" + id;
-		br = document.createElement("br");
-		box.appendChild(text);
-		box.appendChild(span);
-		box.appendChild(br);
-
-		text = document.createTextNode("State: ");
-		span = document.createElement("span");
-		span.id = "state:" + id;
-		br = document.createElement("br");
-		box.appendChild(text);
-		box.appendChild(span);
-		box.appendChild(br);
-
-		text = document.createTextNode("Target seconds: ");
-		span = document.createElement("span");
-		span.id = "targetSeconds:" + id;
-		br = document.createElement("br");
-		box.appendChild(text);
-		box.appendChild(span);
-		box.appendChild(br);
-
-		text = document.createTextNode("Elapsed seconds: ");
-		span = document.createElement("span");
-		span.id = "elapsedSeconds:" + id;
-		br = document.createElement("br");
-		box.appendChild(text);
-		box.appendChild(span);
-		box.appendChild(br);
-
-		text = document.createTextNode("Remaining seconds: ");
-		span = document.createElement("span");
-		span.id = "remainingSeconds:" + id;
-		br = document.createElement("br");
-		box.appendChild(text);
-		box.appendChild(span);
-		box.appendChild(br);
-
-		span = document.createElement("span");
-		span.id = "start:" + id;
-		span.innerHTML = "[start]";
-		box.appendChild(span);
-
-		span = document.createElement("span");
-		span.id = "pause:" + id;
-		span.innerHTML = "[pause]";
-		box.appendChild(span);
-
-		span = document.createElement("span");
-		span.id = "stop:" + id;
-		span.innerHTML = "[stop]";
-		box.appendChild(span);
-	}
-}
-
-function setupUpdates(timers)
-{
-	setInterval(function()
+	Timer.prototype.getId = function()
 	{
+		return this._id;
+	};
+
+	Timer.prototype.setLabel = function(label)
+	{
+		this._label = label;
+	};
+
+	Timer.prototype.getLabel = function()
+	{
+		return this._label;
+	};
+
+	Timer.prototype.setTargetSeconds = function(targetSeconds)
+	{
+		this._targetSeconds = targetSeconds;
+
+		// Changing the target time causes the timer to stop
+		this.stop();
+	};
+
+	Timer.prototype.getTargetSeconds = function()
+	{
+		return this._targetSeconds;
+	};
+
+	Timer.prototype.getState = function()
+	{
+		return this._state;
+	};
+
+	Timer.prototype.getElapsedSeconds = function()
+	{
+		return this._elapsedSeconds;
+	};
+
+	Timer.prototype.getRemainingSeconds = function()
+	{
+		return this._remainingSeconds;
+	};
+
+	timerrific.Timer = Timer;
+
+	// Private functions
+
+	setupInterface = function(timers)
+	{
+		var box;
+		var text;
+		var span;
+		var br;
+
 		for (var i = 0; i < timers.length; i++)
 		{
 			var timer;
@@ -247,58 +177,144 @@ function setupUpdates(timers)
 			timer = timers[i];
 			id = timer.getId();
 
-			document.getElementById("id:" + id).innerHTML = timer.getId();
-			document.getElementById("label:" + id).innerHTML = timer.getLabel();
-			document.getElementById("state:" + id).innerHTML = timer.getState();
-			document.getElementById("targetSeconds:" + id).innerHTML = timer.getTargetSeconds();
-			document.getElementById("elapsedSeconds:" + id).innerHTML = timer.getElapsedSeconds();
-			document.getElementById("remainingSeconds:" + id).innerHTML = timer.getRemainingSeconds();
+			box = document.createElement("div");
+			box.setAttribute("id", "timer:" + id);
+			document.getElementById("container")
+				.appendChild(box);
+
+			text = document.createTextNode("Id: ");
+			span = document.createElement("span");
+			span.id = "id:" + id;
+			br = document.createElement("br");
+			box.appendChild(text);
+			box.appendChild(span);
+			box.appendChild(br);
+
+			text = document.createTextNode("Label: ");
+			span = document.createElement("span");
+			span.id = "label:" + id;
+			br = document.createElement("br");
+			box.appendChild(text);
+			box.appendChild(span);
+			box.appendChild(br);
+
+			text = document.createTextNode("State: ");
+			span = document.createElement("span");
+			span.id = "state:" + id;
+			br = document.createElement("br");
+			box.appendChild(text);
+			box.appendChild(span);
+			box.appendChild(br);
+
+			text = document.createTextNode("Target seconds: ");
+			span = document.createElement("span");
+			span.id = "targetSeconds:" + id;
+			br = document.createElement("br");
+			box.appendChild(text);
+			box.appendChild(span);
+			box.appendChild(br);
+
+			text = document.createTextNode("Elapsed seconds: ");
+			span = document.createElement("span");
+			span.id = "elapsedSeconds:" + id;
+			br = document.createElement("br");
+			box.appendChild(text);
+			box.appendChild(span);
+			box.appendChild(br);
+
+			text = document.createTextNode("Remaining seconds: ");
+			span = document.createElement("span");
+			span.id = "remainingSeconds:" + id;
+			br = document.createElement("br");
+			box.appendChild(text);
+			box.appendChild(span);
+			box.appendChild(br);
+
+			span = document.createElement("span");
+			span.id = "start:" + id;
+			span.innerHTML = "[start]";
+			box.appendChild(span);
+
+			span = document.createElement("span");
+			span.id = "pause:" + id;
+			span.innerHTML = "[pause]";
+			box.appendChild(span);
+
+			span = document.createElement("span");
+			span.id = "stop:" + id;
+			span.innerHTML = "[stop]";
+			box.appendChild(span);
 		}
-	}, 100);
-}
+	};
 
-function setupListeners(timers)
-{
-	var timer;
-
-	for (var i = 0; i < timers.length; i++)
+	setupUpdates = function(timers)
 	{
-		timer = timers[i];
+		setInterval(function()
+		{
+			for (var i = 0; i < timers.length; i++)
+			{
+				var timer;
+				var id;
 
-		document.getElementById("start:" + timer.getId())
-			.addEventListener("click",
-					  timer.start.bind(timer));
-		document.getElementById("pause:" + timer.getId())
-			.addEventListener("click",
-					  timer.pause.bind(timer));
-		document.getElementById("stop:" + timer.getId())
-			.addEventListener("click",
-					  timer.stop.bind(timer));
-	}
-}
+				timer = timers[i];
+				id = timer.getId();
 
-function main()
-{
-	var timers;
-	var timer;
+				document.getElementById("id:" + id).innerHTML = timer.getId();
+				document.getElementById("label:" + id).innerHTML = timer.getLabel();
+				document.getElementById("state:" + id).innerHTML = timer.getState();
+				document.getElementById("targetSeconds:" + id).innerHTML = timer.getTargetSeconds();
+				document.getElementById("elapsedSeconds:" + id).innerHTML = timer.getElapsedSeconds();
+				document.getElementById("remainingSeconds:" + id).innerHTML = timer.getRemainingSeconds();
+			}
+		}, 100);
+	};
 
-	timers = [];
+	setupListeners = function(timers)
+	{
+		var timer;
 
-	timer = new Timer();
-	timer.setId(10);
-	timer.setLabel("10 seconds");
-	timer.setTargetSeconds(10);
-	timers.push(timer);
+		for (var i = 0; i < timers.length; i++)
+		{
+			timer = timers[i];
 
-	timer = new Timer();
-	timer.setId(30);
-	timer.setLabel("30 seconds");
-	timer.setTargetSeconds(30);
-	timers.push(timer);
+			document.getElementById("start:" + timer.getId())
+				.addEventListener("click",
+						  timer.start.bind(timer));
+			document.getElementById("pause:" + timer.getId())
+				.addEventListener("click",
+						  timer.pause.bind(timer));
+			document.getElementById("stop:" + timer.getId())
+				.addEventListener("click",
+						  timer.stop.bind(timer));
+		}
+	};
 
-	setupInterface(timers);
-	setupUpdates(timers);
-	setupListeners(timers);
-}
+	// Exported functions
 
-document.addEventListener("DOMContentLoaded", main);
+	timerrific.main = function()
+	{
+		var timers;
+		var timer;
+
+		timers = [];
+
+		timer = new Timer();
+		timer.setId(10);
+		timer.setLabel("10 seconds");
+		timer.setTargetSeconds(10);
+		timers.push(timer);
+
+		timer = new Timer();
+		timer.setId(30);
+		timer.setLabel("30 seconds");
+		timer.setTargetSeconds(30);
+		timers.push(timer);
+
+		setupInterface(timers);
+		setupUpdates(timers);
+		setupListeners(timers);
+	};
+
+}());
+
+document.addEventListener("DOMContentLoaded", timerrific.main);
