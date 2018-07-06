fc9eddfc4e1463ebbd91db3de4768ed069986652
Andrea Bolognani <eof@kiyuko.org>
2018-07-06 15:45:17 +0200

    Make FAB useful

--- app/src/main/java/org/kiyuko/timerwimer/MainActivity.java
+++ app/src/main/java/org/kiyuko/timerwimer/MainActivity.java
@@ -18,14 +18,25 @@
 
 package org.kiyuko.timerwimer;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(getApplicationContext(), "...", Toast.LENGTH_SHORT).show();
     }
 }
