da6ac3d27b0e15c9999302c00d2cf28b3c4f67d8
Andrea Bolognani <eof@kiyuko.org>
2018-07-06 10:54:19 +0200

    Add Android skeleton

---
+++ app/src/main/java/org/kiyuko/timerwimer/MainActivity.java
@@ -0,0 +1,31 @@
+/* Timer Wimer - Countdown timers for your device
+ * Copyright (C) 2013-2018  Andrea Bolognani <eof@kiyuko.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.kiyuko.timerwimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
