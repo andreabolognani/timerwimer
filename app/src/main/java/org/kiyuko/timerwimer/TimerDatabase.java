fa46543c98447db87603c2b47da43981236f2887
Andrea Bolognani <eof@kiyuko.org>
2018-07-07 17:46:16 +0200

    Add Room database backend

---
+++ app/src/main/java/org/kiyuko/timerwimer/TimerDatabase.java
@@ -0,0 +1,46 @@
+/* Timer Wimer - Countdown timers for your device
+ * Copyright (C) 2013-2018  Andrea Bolognani <eof@kiyuko.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.kiyuko.timerwimer;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {TimerInfo.class}, version = 1)
+public abstract class TimerDatabase extends RoomDatabase {
+
+    public abstract TimerDao getDao();
+
+    private static TimerDatabase INSTANCE;
+
+    static TimerDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (TimerDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TimerDatabase.class, TimerInfo.class.getName())
+                            .build();
+
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
---
+++ app/src/main/java/org/kiyuko/timerwimer/TimerRepository.java
@@ -0,0 +1,59 @@
+/* Timer Wimer - Countdown timers for your device
+ * Copyright (C) 2013-2018  Andrea Bolognani <eof@kiyuko.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package org.kiyuko.timerwimer;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+public class TimerRepository {
+
+    private TimerDao mDao;
+    private LiveData<List<TimerInfo>> mAllTimerInfo;
+
+    public TimerRepository(Application application) {
+        mDao = TimerDatabase.getDatabase(application).getDao();
+        mAllTimerInfo = mDao.getAllTimerInfo();
+    }
+
+    public LiveData<List<TimerInfo>> getAllTimerInfo() {
+        return mAllTimerInfo;
+    }
+
+    public void insert(TimerInfo info) {
+        new AsyncInsertTask(mDao).execute(info);
+    }
+
+    private static class AsyncInsertTask extends AsyncTask<TimerInfo, Void, Void> {
+
+        private TimerDao mDao;
+
+        public AsyncInsertTask(TimerDao dao) {
+            mDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(TimerInfo... args) {
+            mDao.insert(args[0]);
+            return null;
+        }
+    }
+}
